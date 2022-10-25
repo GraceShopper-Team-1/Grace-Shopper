c3791d292e6b8aef8c3af77e2426c35781a538c7
Vi Xiang <vxiang0000@gmail.com>
2022-10-25 10:15:15 -0400

    added books routes

--- server/api/books.js
+++ server/api/books.js
@@ -15,3 +15,43 @@ router.get("/", async (req, res, next) => {
 });
 
 // GET api/books/:bookId
+router.get("/:bookId", async (req, res, next) => {
+	try {
+		const book = await Book.findByPk(req.params.bookId);
+		res.json(book);
+	} catch (err) {
+		next(err);
+	}
+});
+
+// POST api/books
+router.post("/", async (req, res, next) => {
+	try {
+		const book = await Book.create(req.body);
+		res.status(201).json(book);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// PUT /api/books/:bookId
+router.put("/:bookId", async (req, res, next) => {
+	try {
+		const book = await Book.findByPk(req.params.bookId);
+		res.json(await book.update(req.body));
+	} catch (error) {
+		next(error);
+	}
+});
+
+// DELETE /api/books/:bookId
+router.delete("/:bookId", async (req, res, next) => {
+	try {
+		const book = await Book.findByPk(req.params.bookId)
+		await book.destroy();
+		res.sendStatus(204);
+	} catch (error) {
+		next(error);
+	}
+});
+
