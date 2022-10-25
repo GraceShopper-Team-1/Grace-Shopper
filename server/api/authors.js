a3e110483018ded6f263cab2132ff7f2c0f23b58
Vi Xiang <vxiang0000@gmail.com>
2022-10-25 10:16:28 -0400

    added author routes

--- server/api/authors.js
+++ server/api/authors.js
@@ -15,3 +15,43 @@ router.get("/", async (req, res, next) => {
 });
 
 // GET api/authors/:authorId
+router.get("/:authorId", async (req, res, next) => {
+	try {
+		const author = await Author.findByPk(req.params.authorId);
+		res.json(author);
+	} catch (err) {
+		next(err);
+	}
+});
+
+// POST api/authors
+router.post("/", async (req, res, next) => {
+	try {
+		const author = await Author.create(req.body);
+		res.status(201).json(author);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// PUT /api/authors/:authorId
+router.put("/:authorId", async (req, res, next) => {
+	try {
+		const author = await Author.findByPk(req.params.authorId);
+		res.json(await author.update(req.body));
+	} catch (error) {
+		next(error);
+	}
+});
+
+// DELETE /api/authors/:authorId
+router.delete("/:authorId", async (req, res, next) => {
+	try {
+		const author = await Author.findByPk(req.params.authorId)
+		await author.destroy();
+		res.sendStatus(204);
+	} catch (error) {
+		next(error);
+	}
+});
+
