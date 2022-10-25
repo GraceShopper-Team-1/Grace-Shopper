600939c88f417d214938de8052eadc93e6f6d927
Andrew Ozoria <andrewozo123@gmail.com>
2022-10-25 14:56:10 -0400

    seeded database

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,51 @@
-'use strict'
+"use strict";
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Book },
+} = require("../server/db");
+const axios = require("axios");
+
+const key = "LAb33WvyGlQbTJuFxzvopNpRoK8dgaKH";
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
+
+  const { data } = await axios.get(
+    `https://api.nytimes.com/svc/books/v3/lists/2013-04-20/hardcover-fiction.json?api-key=${key}`
+  );
+  const books = data.results.books;
+
+  books.map((book) => {
+    Book.create({
+      title: book.title,
+      author: book.author,
+      coverImage: book.book_image,
+      description: book.description,
+      isbn: book.primary_isbn10,
+      price: parseInt(book.price),
+    });
+  });
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+    User.create({ username: "cody", password: "123" }),
+    User.create({ username: "murphy", password: "123" }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+  };
 }
 
 /*
@@ -32,16 +54,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -51,8 +73,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
