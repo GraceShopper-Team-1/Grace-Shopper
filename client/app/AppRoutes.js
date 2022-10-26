e91b953bcf2f9c1864e2ca3852f916bda4f58ae0
Breana Johnson <brecjohnson@gmail.com>
2022-10-26 17:55:15 -0400

    updated slice code

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -6,7 +6,6 @@ import AuthForm from "../features/auth/AuthForm";
 import Home from "../features/home/Home";
 import SingleProduct from "../features/singleProduct/SingleProduct";
 import Cart from "../features/cart/Cart";
-import LoadingScreen from "../features/LoadingScreen/LoadingScreen";
 import { me } from "./store";
 
 /**
@@ -14,45 +13,41 @@ import { me } from "./store";
  */
 
 const AppRoutes = () => {
-	const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-	const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(me());
-	}, []);
+  useEffect(() => {
+    dispatch(me());
+  }, []);
 
-	return (
-		<div>
-			{isLoggedIn ? (
-				<Routes>
-					<Route path="/*" element={<Home />} />
-					<Route to="/home" element={<Home />} />
-					<Route path="/products" element={<AllProducts />} />
-					<Route path="/products/:productId" element={<SingleProduct />} />
-					<Route path="/cart" element={<Cart />} />
-          <Route path="loading" element={<LoadingScreen/>} />
-					<Route path="*" element={"Not found!"} />
-				</Routes>
-			) : (
-				<Routes>
-					<Route
-						path="/*"
-						element={<AuthForm name="login" displayName="Login" />}
-					/>
-					<Route
-						path="/login"
-						element={<AuthForm name="login" displayName="Login" />}
-					/>
-					<Route
-						path="/signup"
-						element={<AuthForm name="signup" displayName="Sign Up" />}
-					/>
-
-					<Route path="/products/:productId" element={<SingleProduct />} />
-				</Routes>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {isLoggedIn ? (
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route to="/home" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:productId" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={"Not found!"} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route
+            path="/login"
+            element={<AuthForm name="login" displayName="Login" />}
+          />
+          <Route
+            path="/signup"
+            element={<AuthForm name="signup" displayName="Sign Up" />}
+          />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:productId" element={<SingleProduct />} />
+        </Routes>
+      )}
+    </div>
+  );
 };
 
 export default AppRoutes;
