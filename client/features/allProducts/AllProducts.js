c22c606b40544a27aef0a67a8c090a3b57f559ff
Andrew Ozoria <andrewozo123@gmail.com>
2022-10-27 15:20:06 -0400

    singleProduct change 2

--- client/features/allProducts/AllProducts.js
+++ client/features/allProducts/AllProducts.js
@@ -1,45 +1,45 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchAllProducts } from "./allProductsSlice";
+import { fetchAllProducts } from "../allProducts/allProductsSlice";
 import { addToCart } from "../cart/cartSlice";
 
 function AllProducts() {
-	const dispatch = useDispatch();
-	const products = useSelector((state) => state.allProducts.allProducts);
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.allProducts.allProducts);
 
-	useEffect(() => {
-		dispatch(fetchAllProducts());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, [dispatch]);
 
-	const handleAddToCart = (product) => {
-		dispatch(addToCart(product));
-	};
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+  };
 
-	return (
-		<div id="all-products">
-			<h3>Bestsellers</h3>
-			<div className="column-container">
-				{products.map((product) => (
-					<div className="product-entry" key={product.id}>
-						<Link to={`/products/${product.id}`}>
-							<img
-								src={product.coverImage}
-								alt="Cover Image"
-								className="product-img"
-							/>
-							<h3>{product.title}</h3>
-							<h5>{product.author}</h5>
-							<p>${product.price}</p>
-						</Link>
-						<button type="submit" onClick={() => handleAddToCart(product)}>
-							Add to cart
-						</button>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div id="all-products">
+      <h3>Bestsellers</h3>
+      <div className="column-container">
+        {products.map((product) => (
+          <div className="product-entry" key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              <img
+                src={product.coverImage}
+                alt="Cover Image"
+                className="product-img"
+              />
+              <h3>{product.title}</h3>
+              <h5>{product.author}</h5>
+              <p>${product.price}</p>
+            </Link>
+            <button type="submit" onClick={() => handleAddToCart(product)}>
+              Add to cart
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default AllProducts;
