81abcc0b904e7f20d7616e1fc2717d64f56e6f77
Vi Xiang <vxiang0000@gmail.com>
2022-10-27 16:07:40 -0400

    admin for products routes

--- client/features/cart/cartSlice.js
+++ client/features/cart/cartSlice.js
@@ -2,32 +2,32 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchCart = createAsyncThunk("cart/fetchAll", async () => {
-  const { data } = await axios.get("/api/cart");
-  return data;
+	const { data } = await axios.get("/api/cart");
+	return data;
 });
 
 // split into POST and PUT for more than one click?
 // updating order_products db, testing with orderId 1
 export const addToCart = createAsyncThunk(
-  "cart/add",
-  async ({ id, title, author, coverImage, price }) => {
-    const { data } = await axios.post("/api/cart", {
-      title,
-      author,
-      coverImage,
-      price,
-      id,
-      productId: id,
-      orderId: 1,
-    });
-    console.log("data", data);
-    return data;
-  }
+	"cart/add",
+	async ({ id, title, author, coverImage, price }) => {
+		const { data } = await axios.post("/api/cart", {
+			title,
+			author,
+			coverImage,
+			price,
+			id,
+			productId: id,
+			orderId: 1,
+		});
+		console.log("data", data);
+		return data;
+	}
 );
 
 export const removeFromCart = createAsyncThunk("removeFromCart", async (id) => {
-  const { data } = await axios.delete(`/api/cart/${id}`);
-  return id;
+	const { data } = await axios.delete(`/api/cart/${id}`);
+	return id;
 });
 
 // export const updateCart = createAsyncThunk("cart/update", async (productId) => {
@@ -37,38 +37,38 @@ export const removeFromCart = createAsyncThunk("removeFromCart", async (id) => {
 // });
 
 const initialState = {
-  cart: [],
+	cart: [],
 };
 
 const cartSlice = createSlice({
-  name: "cart",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchCart.fulfilled, (state, action) => {
-        state.cart = action.payload;
-      })
-      .addCase(addToCart.fulfilled, (state, action) => {
-        const itemInCart = state.cart.find(
-          (item) => item.id === action.payload.id
-        );
-        if (itemInCart) {
-          itemInCart.purchaseQuantity++;
-        } else {
-          state.cart.push({ ...action.payload, purchaseQuantity: 1 });
-        }
-      })
-      .addCase(removeFromCart.fulfilled, (state, action) => {
-        return state.cart.filter((cart) => cart.id !== action.payload);
-      });
-    // .addCase(updateCart.fulfilled, (state, action) => {
-    // 	const itemInCart = state.cart.find(
-    // 		(item) => item.id === action.payload.id
-    // 	);
-    // 	itemInCart.purchaseQuantity++;
-    // });
-  },
+	name: "cart",
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchCart.fulfilled, (state, action) => {
+				state.cart = action.payload;
+			})
+			.addCase(addToCart.fulfilled, (state, action) => {
+				const itemInCart = state.cart.find(
+					(item) => item.id === action.payload.id
+				);
+				if (itemInCart) {
+					itemInCart.purchaseQuantity++;
+				} else {
+					state.cart.push({ ...action.payload, purchaseQuantity: 1 });
+				}
+			})
+			.addCase(removeFromCart.fulfilled, (state, action) => {
+				state.cart = state.cart.filter((cart) => cart.id !== action.payload);
+			});
+		// .addCase(updateCart.fulfilled, (state, action) => {
+		// 	const itemInCart = state.cart.find(
+		// 		(item) => item.id === action.payload.id
+		// 	);
+		// 	itemInCart.purchaseQuantity++;
+		// });
+	},
 });
 
 export default cartSlice.reducer;
