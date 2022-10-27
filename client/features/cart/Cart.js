11ea12eff3509ecf55d00bcbcf352bbe69ce59f5
Andrew Ozoria <andrewozo123@gmail.com>
2022-10-27 11:01:18 -0400

    remove from cart and external database

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -1,43 +1,52 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { fetchCart } from "./cartSlice";
+import { Link, useParams } from "react-router-dom";
+import { fetchCart, removeFromCart } from "./cartSlice";
 
 function Cart() {
-	const dispatch = useDispatch();
-	const cart = useSelector((state) => state.cart.cart);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(fetchCart());
-	}, [dispatch]);
+  const cart = useSelector((state) => state.cart.cart);
 
-	return (
-		<div id="shopping-cart">
-			<h3>Shopping Cart</h3>
-			<div className="column-container">
-				{cart.map((cartItem) => (
-					<div className="product-entry" key={cartItem.id}>
-						<Link to={`/products/${cartItem.id}`}>
-							<img
-								src={cartItem.coverImage}
-								alt="Cover Image"
-								className="product-img"
-							/>
-							<h3>{cartItem.title}</h3>
-							<h5>{cartItem.author}</h5>
-							<p>${cartItem.price}</p>
-							<p>Quantity: {cartItem.purchaseQuantity}</p>
-						</Link>
-						<button type="button">Remove from cart</button>
-					</div>
-				))}
-			</div>
-			<div>
-				<h3>Order details:</h3>
-				<button type="submit">Checkout</button>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    dispatch(fetchCart());
+  }, [dispatch]);
+
+  return (
+    <div id="shopping-cart">
+      <h3>Shopping Cart</h3>
+      <div className="column-container">
+        {cart.map((cartItem) => (
+          <div className="product-entry" key={cartItem.id}>
+            <Link to={`/products/${cartItem.productId}`}>
+              <img
+                src={cartItem.coverImage}
+                alt="Cover Image"
+                className="product-img"
+              />
+              <h3>{cartItem.title}</h3>
+              <h5>{cartItem.author}</h5>
+              <p>${cartItem.price}</p>
+              <p>Quantity: {cartItem.purchaseQuantity}</p>
+            </Link>
+            <button
+              type="delete"
+              onClick={async () => {
+                await dispatch(removeFromCart(cartItem.productId));
+                await dispatch(fetchCart());
+              }}
+            >
+              Remove from cart
+            </button>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h3>Order details:</h3>
+        <button type="submit">Checkout</button>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
--- client/features/cart/cartSlice.js
+++ client/features/cart/cartSlice.js
@@ -2,28 +2,33 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchCart = createAsyncThunk("cart/fetchAll", async () => {
-	const { data } = await axios.get("/api/cart");
-	return data;
+  const { data } = await axios.get("/api/cart");
+  return data;
 });
 
 // split into POST and PUT for more than one click?
 // updating order_products db, testing with orderId 1
 export const addToCart = createAsyncThunk(
-	"cart/add",
-	async ({ id, title, author, coverImage, price }) => {
-		const { data } = await axios.post("/api/cart", {
-			title,
-			author,
-			coverImage,
-			price,
-			id,
-			productId: id,
-			orderId: 1,
-		});
-		console.log("data", data);
-		return data;
-	}
+  "cart/add",
+  async ({ id, title, author, coverImage, price }) => {
+    const { data } = await axios.post("/api/cart", {
+      title,
+      author,
+      coverImage,
+      price,
+      id,
+      productId: id,
+      orderId: 1,
+    });
+    console.log("data", data);
+    return data;
+  }
 );
+
+export const removeFromCart = createAsyncThunk("removeFromCart", async (id) => {
+  const { data } = await axios.delete(`/api/cart/${id}`);
+  return id;
+});
 
 // export const updateCart = createAsyncThunk("cart/update", async (productId) => {
 // 	const { data } = await axios.post("/api/cart", { productId, orderId: 1 }); // updating order_products db, testing with orderId 1
@@ -32,35 +37,38 @@ export const addToCart = createAsyncThunk(
 // });
 
 const initialState = {
-	cart: [],
+  cart: [],
 };
 
 const cartSlice = createSlice({
-	name: "cart",
-	initialState,
-	reducers: {},
-	extraReducers: (builder) => {
-		builder
-			.addCase(fetchCart.fulfilled, (state, action) => {
-				state.cart = action.payload;
-			})
-			.addCase(addToCart.fulfilled, (state, action) => {
-				const itemInCart = state.cart.find(
-					(item) => item.id === action.payload.id
-				);
-				if (itemInCart) {
-					itemInCart.purchaseQuantity++;
-				} else {
-					state.cart.push({ ...action.payload, purchaseQuantity: 1 });
-				}
-			});
-		// .addCase(updateCart.fulfilled, (state, action) => {
-		// 	const itemInCart = state.cart.find(
-		// 		(item) => item.id === action.payload.id
-		// 	);
-		// 	itemInCart.purchaseQuantity++;
-		// });
-	},
+  name: "cart",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCart.fulfilled, (state, action) => {
+        state.cart = action.payload;
+      })
+      .addCase(addToCart.fulfilled, (state, action) => {
+        const itemInCart = state.cart.find(
+          (item) => item.id === action.payload.id
+        );
+        if (itemInCart) {
+          itemInCart.purchaseQuantity++;
+        } else {
+          state.cart.push({ ...action.payload, purchaseQuantity: 1 });
+        }
+      })
+      .addCase(removeFromCart.fulfilled, (state, action) => {
+        state.cart.filter((cart) => cart.id !== action.payload);
+      });
+    // .addCase(updateCart.fulfilled, (state, action) => {
+    // 	const itemInCart = state.cart.find(
+    // 		(item) => item.id === action.payload.id
+    // 	);
+    // 	itemInCart.purchaseQuantity++;
+    // });
+  },
 });
 
 export default cartSlice.reducer;
