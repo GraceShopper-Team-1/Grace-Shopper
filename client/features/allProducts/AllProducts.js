d9ef3b6deeb27b43f949829f9810d74694000c56
Vi Xiang <vxiang0000@gmail.com>
2022-10-30 13:56:26 -0400

    bllProductsSlice

--- client/features/allProducts/AllProducts.js
+++ client/features/allProducts/AllProducts.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchAllProducts } from "./AllProductsSlice";
+import { fetchAllProducts } from "./bllProductsSlice";
 import { addToCart } from "../cart/cartSlice";
 
 function AllProducts() {
