efa199d88e2d03664e197c5683cd36dec2b0254e
Vi Xiang <vxiang0000@gmail.com>
2022-10-30 13:56:55 -0400

    allProductsSlice lowercase

--- client/features/allProducts/AllProducts.js
+++ client/features/allProducts/AllProducts.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchAllProducts } from "./bllProductsSlice";
+import { fetchAllProducts } from "./allProductsSlice";
 import { addToCart } from "../cart/cartSlice";
 
 function AllProducts() {
