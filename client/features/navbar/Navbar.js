9c4ebcdf8eebc91195aced8a4d75cf689f6cbffb
Vi Xiang <vxiang0000@gmail.com>
2022-10-30 13:44:40 -0400

    react router for products dash

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -21,7 +21,24 @@ const Navbar = () => {
 				<h1>Bookworm</h1>
 			</Link>
 			<nav>
-				{isLoggedIn ? (
+				{isLoggedIn && isAdmin ? (
+					<div className="navbar">
+						{/* The navbar will show these links after you log in */}
+						<Link to="/home">Home</Link>
+						<Link to="/products">Browse</Link>
+						<Link to="/products-dashboard">Products Dashboard</Link>
+						<Link to="/cart">
+							<img
+								className="cart-icon"
+								src="https://static-00.iconduck.com/assets.00/shopping-cart-icon-512x462-yrde1eu0.png"
+							/>
+						</Link>
+						<h4>Welcome Admin {username}</h4>
+						<button type="button" onClick={logoutAndRedirectHome}>
+							Logout
+						</button>
+					</div>
+				) : isLoggedIn ? (
 					<div className="navbar">
 						{/* The navbar will show these links after you log in */}
 						<Link to="/home">Home</Link>
@@ -32,7 +49,7 @@ const Navbar = () => {
 								src="https://static-00.iconduck.com/assets.00/shopping-cart-icon-512x462-yrde1eu0.png"
 							/>
 						</Link>
-						<h4>Welcome{username ? `, ${username}` : " to Bookworm"}</h4>
+						<h4>Welcome {username}</h4>
 						<button type="button" onClick={logoutAndRedirectHome}>
 							Logout
 						</button>
@@ -47,6 +64,7 @@ const Navbar = () => {
 								src="https://static-00.iconduck.com/assets.00/shopping-cart-icon-512x462-yrde1eu0.png"
 							/>
 						</Link>
+						<h4>Welcome to Bookworm</h4>
 						<Link to="/login">Login</Link>
 						<Link to="/signup">Sign Up</Link>
 					</div>
