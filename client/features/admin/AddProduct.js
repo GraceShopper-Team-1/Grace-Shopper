ee3cb314d2cdb8d9ae7ddb372674e5b1d1d1a6d9
Vi Xiang <vxiang0000@gmail.com>
2022-10-31 09:52:34 -0400

    small LInk in Addproduct

--- client/features/admin/AddProduct.js
+++ client/features/admin/AddProduct.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { addProduct } from "../allProducts/allProductsSlice";
 
 const AddProduct = () => {
