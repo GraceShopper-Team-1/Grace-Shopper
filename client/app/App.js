1dd6d547cfc50a1b621ff8200f85e0f9e410b47f
Vi Xiang <vxiang0000@gmail.com>
2022-11-02 19:14:52 -0400

    fixed guest

--- client/app/App.js
+++ client/app/App.js
@@ -3,7 +3,9 @@ import Navbar from "../features/navbar/Navbar";
 import AppRoutes from "./AppRoutes";
 
 const App = () => {
-	localStorage.setItem("guest", JSON.stringify([]));
+	if (!localStorage.getItem("guest")) {
+		localStorage.setItem("guest", JSON.stringify([]));
+	}
 	return (
 		<div>
 			<Navbar />
