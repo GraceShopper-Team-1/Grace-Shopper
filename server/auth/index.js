ff7e6cec4a0f6c3129f1dd79bffdf9feca718b7f
Alejandro Baez <abaez2623@gmail.com>
2022-11-02 17:59:20 -0400

    Fixed user login

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,36 +1,35 @@
 const router = require("express").Router();
 const {
-	models: { User },
+  models: { User },
 } = require("../db");
 module.exports = router;
 
 router.post("/login", async (req, res, next) => {
-	try {
-		res.send({ token: await User.authenticate(req.body) });
-	} catch (err) {
-		next(err);
-	}
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/signup", async (req, res, next) => {
-	try {
-		const { username, password, email } = req.body;
-		const user = await User.create({ username, password, email });
-		res.send({ token: await user.generateToken() });
-	} catch (err) {
-		if (err.name === "SequelizeUniqueConstraintError") {
-			res.status(401).send("User already exists");
-		} else {
-			next(err);
-		}
-	}
+  try {
+    const { username, password, email } = req.body;
+    const user = await User.create({ username, password, email });
+    res.send({ token: await user.generateToken() });
+  } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      res.status(401).send("User already exists");
+    } else {
+      next(err);
+    }
+  }
 });
 
 router.get("/me", async (req, res, next) => {
-	try {
-   
-		res.status(204).send(await User.findByToken(req.headers.authorization));
-	} catch (ex) {
-		next(ex);
-	}
+  try {
+    res.send(await User.findByToken(req.headers.authorization));
+  } catch (ex) {
+    next(ex);
+  }
 });
