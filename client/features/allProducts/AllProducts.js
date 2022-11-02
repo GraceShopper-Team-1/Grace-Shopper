9febe109d6bf3852165d17334513d32ad8974975
Andrew Ozoria <andrewozo123@gmail.com>
2022-11-02 11:44:09 -0400

    single add to cart

--- client/features/allProducts/AllProducts.js
+++ client/features/allProducts/AllProducts.js
@@ -6,8 +6,11 @@ import { addToCart } from "../cart/cartSlice";
 
 function AllProducts() {
   const dispatch = useDispatch();
+
   const products = useSelector((state) => state.allProducts.products);
+
   const userId = useSelector((state) => state.auth.me.id);
+  
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
 
   useEffect(() => {
@@ -15,19 +18,19 @@ function AllProducts() {
   }, [dispatch]);
 
   const handleAddToCart = (productId) => {
-   
     dispatch(addToCart({ productId }));
   };
   let productArr = [];
   const handleGuestCart = (product) => {
     productArr.push(product);
-    
+
     localStorage.setItem("guest", JSON.stringify(productArr));
   };
 
   return (
     <div id="all-products">
       <h3>Bestsellers</h3>
+
       <div className="column-container">
         {products.map((product) => (
           <li key={product.id} className="product-entry">
@@ -37,8 +40,11 @@ function AllProducts() {
                 alt="Cover Image"
                 className="product-img"
               />
+
               <h3>{product.title}</h3>
+
               <h5>{product.author}</h5>
+
               <p>${product.price}</p>
             </Link>
             <button
