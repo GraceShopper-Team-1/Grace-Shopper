bcb6681582d87a25c78e15dc188b4284a328fc78
Vi Xiang <vxiang0000@gmail.com>
2022-11-02 18:03:07 -0400

    fixed

--- client/app/App.js
+++ client/app/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Navbar from '../features/navbar/Navbar';
-import AppRoutes from './AppRoutes';
+import React from "react";
+import Navbar from "../features/navbar/Navbar";
+import AppRoutes from "./AppRoutes";
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <AppRoutes />
-    </div>
-  );
+	localStorage.setItem("guest", JSON.stringify([]));
+	return (
+		<div>
+			<Navbar />
+			<AppRoutes />
+		</div>
+	);
 };
 
 export default App;
