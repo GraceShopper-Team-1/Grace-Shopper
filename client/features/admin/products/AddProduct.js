fc38ddd046de0c7070c9cd16df3dffbe9d958615
Andrew Ozoria <andrewozo123@gmail.com>
2022-11-02 12:23:25 -0400

    fixed add new book

--- client/features/admin/products/AddProduct.js
+++ client/features/admin/products/AddProduct.js
@@ -1,66 +1,68 @@
+import { use } from "chai";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { addProduct } from "../../allProducts/allProductsSlice";
 
 const AddProduct = () => {
-	const [title, setTitle] = useState("");
-	const [author, setAuthor] = useState("");
-	const [coverImageUrl, setCoverImageUrl] = useState("");
-	const [price, setPrice] = useState(0);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [coverImageUrl, setCoverImageUrl] = useState("");
+  const [price, setPrice] = useState(0);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		dispatch(addProduct({ title, author, coverImageUrl, price }));
-		setTitle("");
-		setAuthor("");
-		setCoverImageUrl("");
-		setPrice(0);
-	};
+  const navigate = useNavigate();
 
-	return (
-		<div className="sticky">
-			<h3 className="gray">Add new entry</h3>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="title">Title:</label>
-				<input
-					type="text"
-					name="title"
-					value={title}
-					onChange={(event) => setTitle(event.target.value)}
-				/>
-				<label htmlFor="author">Author:</label>
-				<input
-					type="text"
-					name="author"
-					value={author}
-					onChange={(event) => setAuthor(event.target.value)}
-				/>
-				<label htmlFor="coverImageUrl">Cover Image URL:</label>
-				<input
-					type="text"
-					name="coverImageUrl"
-					value={coverImageUrl}
-					onChange={(event) => setCoverImageUrl(event.target.value)}
-				/>
-				<label htmlFor="price">Price:</label>
-				<input
-					type="text"
-					name="price"
-					value={price}
-					onChange={(event) => setPrice(event.target.value)}
-				/>
-				<br />
-				<br />
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await dispatch(addProduct({ title, author, coverImageUrl, price }));
+    setTitle("");
+    setAuthor("");
+    setCoverImageUrl("");
+    setPrice(0);
+    navigate("/admin/products");
+  };
 
-				<Link to="/admin/products">
-					<button type="submit">Submit</button>
-				</Link>
-			</form>
-		</div>
-	);
+  return (
+    <div className="sticky">
+      <h3 className="gray">Add new entry</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label htmlFor="author">Author:</label>
+        <input
+          type="text"
+          name="author"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        />
+        <label htmlFor="coverImageUrl">Cover Image URL:</label>
+        <input
+          type="text"
+          name="coverImageUrl"
+          value={coverImageUrl}
+          onChange={(event) => setCoverImageUrl(event.target.value)}
+        />
+        <label htmlFor="price">Price:</label>
+        <input
+          type="text"
+          name="price"
+          value={price}
+          onChange={(event) => setPrice(event.target.value)}
+        />
+        <br />
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddProduct;
